b6d9eeac7a95c73f37e267d75cfd4b76c67fd173
Nachiekt Kale <nachiket.kale@mail.mcgill.ca>
2020-01-30 14:09:17 -0500

    check if lines on x axis overlap

---
+++ Nachiket_kale_testA.py
@@ -0,0 +1,24 @@
+#Check whether two lines on x- axis overlap or not
+#User input for first point
+while True:
+    try:
+#This code takes single input at a time to avoide any human error which may 
+        x1=float(input('Enter the first cordinate of first line: '))
+        x2=float(input('Enter the second cordinate of first line: '))
+        x3=float(input('Enter the sfirst cordinate of second line: '))
+        x4=float(input('Enter the second cordinate of second line: '))
+        
+#This code takes a tuple as a input      
+        #(x1,x2)=tuple(float(x) for x in input('Enter cordinates of first line  : ').split(','))
+        #(x3,x4)=tuple(float(x) for x in input('Enter cordinates of second line : ').split(','))
+        break
+    except:
+        print("That's not a valid option!")
+# Conditions to check if lines are overlaping or not
+if (((x3-x1)/(x2-x1)) or ((x4-x1)/(x2-x1))):
+    print('Lines are overlaping.')
+else:
+    print('Lines are not overlaping')
+    
+
+
